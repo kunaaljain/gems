744454b9a315beaf4f6a65440bf617e4db251d6e
Venkat Arun <venkatarun95@gmail.com>
2015-04-15 13:12:08 +0530

    Integrated voting into the system and made other changes

--- src/dummydata.py
+++
@@ -1,4 +0,0 @@
-from databaseManager.py import addUser, addCandidate, addVotes, addChallengeStrings, addPublicKeys
-addUser('kayush', False, 'cse' , 'Ayush', 'btech','qweqwrqwr');
-#addCandidate('sudhanshu2013', 'resides in umium hostel at room no a3-56', '/static/sudhanshu.jpg',' ');
-#addVotes('["yes",]')
